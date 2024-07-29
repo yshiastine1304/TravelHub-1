16dd3533cad6e4a79834e160271a0abc32f6d20e
Kamiko2021 <klopez_ccs@uspf.edu.ph>
2024-07-29 10:44:30 +0800

    registration form ui

---
+++ app/src/main/java/com/personal/development/travelhub/Registration_view.java
@@ -0,0 +1,14 @@
+package com.personal.development.travelhub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registration_view extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_view);
+    }
+}
