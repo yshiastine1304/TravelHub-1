a3ab65d9477d07bc708054fac84a2b6ff83d099b
Kamiko2021 <klopez_ccs@uspf.edu.ph>
2024-11-16 16:44:44 +0800

    populate destination

--- app/src/main/java/com/personal/development/travelhub/AddToursActivity.java
+++ app/src/main/java/com/personal/development/travelhub/AddToursActivity.java
@@ -3,14 +3,21 @@ package com.personal.development.travelhub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class AddToursActivity extends AppCompatActivity {
@@ -19,13 +26,14 @@ public class AddToursActivity extends AppCompatActivity {
             description_txtV,
             location_txtV,
             inclusionDetails_txtV,
-            destinationDetails_txtV,
+
             activityDetails_txtV,
             duration_txtV,
             price_txtV,
             minimumAge_txtV,
             pricePer_txtV,
             otherDetails_txtV;
+    private Spinner destinationDetails_spinner;
     private Button saveBtn;
     private FirebaseFirestore db;
 
@@ -37,13 +45,13 @@ public class AddToursActivity extends AppCompatActivity {
         setContentView(R.layout.add_tours);
 
         // Initialize FirebaseFirestore instance
-        db = FirebaseFirestore  .getInstance();
+        db = FirebaseFirestore.getInstance();
 
         tourName_txtV = findViewById(R.id.tour_name);
         description_txtV = findViewById(R.id.description_admin_tour);
         location_txtV = findViewById(R.id.location_admin);
         inclusionDetails_txtV = findViewById(R.id.inclusion_details_admin);
-        destinationDetails_txtV = findViewById(R.id.destinations_tour_admin);
+        destinationDetails_spinner = findViewById(R.id.destinations_tour_admin);
         activityDetails_txtV = findViewById(R.id.activity_details_tour_admin);
         duration_txtV = findViewById(R.id.duration_tour_admin);
         price_txtV = findViewById(R.id.price_tour_admin);
@@ -58,14 +66,60 @@ public class AddToursActivity extends AppCompatActivity {
                 saveTour();
             }
         });
+        populateDestinationDetails();
     }
+
+    public void populateDestinationDetails() {
+        List<String> destinationsList = new ArrayList<>();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(AddToursActivity.this, android.R.layout.simple_spinner_item, destinationsList);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        destinationDetails_spinner.setAdapter(adapter);
+
+        db.collection("attractions")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            if (document.exists()) {
+                                String destinationName = document.getString("destination_name");
+                                if (destinationName != null) {
+                                    destinationsList.add(destinationName);
+                                } else {
+                                    Toast.makeText(AddToursActivity.this, "No data found", Toast.LENGTH_SHORT).show();
+                                }
+                            } else {
+                                Toast.makeText(AddToursActivity.this, "Failed to get information", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        adapter.notifyDataSetChanged();
+                    } else {
+                        Toast.makeText(AddToursActivity.this, "Error: "+ task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        destinationDetails_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedDestination = destinationsList.get(postion);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+
+
 
     public void saveTour(){
         String tourname = tourName_txtV.getText().toString();
         String description = description_txtV.getText().toString();
         String location = location_txtV.getText().toString();
         String inclusionDetails = inclusionDetails_txtV.getText().toString();
-        String destinationDetails = destinationDetails_txtV.getText().toString();
+        String destinationDetails = destinationDetails_spinner.getSelectedItem().toString();
         String activityDetails = activityDetails_txtV.getText().toString();
         String duration = duration_txtV.getText().toString();
         String price = price_txtV.getText().toString();
